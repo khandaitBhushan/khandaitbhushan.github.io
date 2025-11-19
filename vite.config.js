a5b73df60ac050abf1d394a122e9c167a8365249
khandaitBhushan <khandaitbhushan6@gmail.com>
2025-11-19 22:21:29 +0530

    Fix resume link and update portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/'   // IMPORTANT
-})
+  base: "/",
+});
